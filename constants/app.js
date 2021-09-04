97ee6ea95678a49f2d58938edb9ae8e19398bd27
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-09-05 01:16:45 +0300

    refactor: move env variables to app constants

---
+++ constants/app.js
@@ -0,0 +1,5 @@
+export const APP_TITLE = 'Leo Messi Space';
+export const APP_DESCRIPTION =
+  'This site brings you the absolutely amazing career of the greatest footballer ever';
+
+export const API_URL = 'http://localhost:3001';
--- features/HtmlHead/HtmlHead.js
+++ features/HtmlHead/HtmlHead.js
@@ -1,11 +1,14 @@
 import Head from 'next/head';
+import { APP_TITLE, APP_DESCRIPTION } from 'constants/app';
 
-export default function HtmlHead({ title, description = '' }) {
+export default function HtmlHead({ title, description = APP_DESCRIPTION }) {
   return (
     <Head>
       <link rel='icon' href='/favicon.ico' />
       <meta name='description' content={description} />
-      <title>{title} &bull; {process.env.NEXT_PUBLIC_APP_TITLE}</title>
+      <title>
+        {title} &bull; {APP_TITLE}
+      </title>
     </Head>
   );
 }
