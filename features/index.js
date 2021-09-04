3b29d28693487f31cd6918695b9c59fc1fb13570
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-09-05 00:20:37 +0300

    refactor: move HtmlHead to features

--- features/index.js
+++ features/index.js
@@ -1,5 +1,6 @@
 export { Header } from './Header';
 export { PageHeader } from './PageHeader';
+export { HtmlHead } from './HtmlHead';
 export { MainMenu } from './MainMenu';
 export { GamesTable } from './GamesTable';
 export { GamePageHeader } from './GamePageHeader';
