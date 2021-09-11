1474057611fb2652445a7e0e671d7edddcc61a2a
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-09-11 21:38:29 +0300

    refactor: update GamesTable

--- features/GamesTable/GamesTable.js
+++ features/GamesTable/GamesTable.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import styles from 'styles/GamesTable.module.scss';
+import { Tag } from 'components';
+import styles from './GamesTable.module.scss';
 
 export default function GamesTable({ games }) {
   return (
@@ -7,8 +8,8 @@ export default function GamesTable({ games }) {
       <thead>
         <tr>
           <th>Date</th>
-          <th>Game</th>
           <th></th>
+          <th>Game</th>
           <th className={styles.resultCell}>Result</th>
         </tr>
       </thead>
@@ -23,23 +24,29 @@ export default function GamesTable({ games }) {
 
 function GameRow({ game }) {
   const { id, team, opponent, field, datetime, competition, result } = game;
-  const title = field === 1 ? `${team.name} – ${opponent.name}` : `${opponent.name} – ${team.name}`;
-  const [homeResult, awayResult] = field === 1 ? result.total : result.total.slice().reverse();
+  const title =
+    field === 1
+      ? `${team.name} – ${opponent.name}`
+      : `${opponent.name} – ${team.name}`;
+  const [homeResult, awayResult] =
+    field === 1 ? result.total : result.total.slice().reverse();
 
   return (
     <tr>
       <td className={styles.dateTimeCell}>{datetime.slice(0, 10)}</td>
+      <td>
+        <Tag size='small'>{competition.name}</Tag>
+      </td>
       <td className={styles.titleCell}>
         <Link href={`/games/${id}`}>
           <a>{title}</a>
         </Link>
       </td>
-      <td className={styles.competitionCell}>
-        <span className={styles.competitionTag}>{competition.name}</span>
-      </td>
       <td className={styles.resultCell}>
         <Link href={`/games/${id}`}>
-          <a className={styles.resultButton}>{`${homeResult} : ${awayResult}`}</a>
+          <a
+            className={styles.resultButton}
+          >{`${homeResult} : ${awayResult}`}</a>
         </Link>
       </td>
     </tr>
